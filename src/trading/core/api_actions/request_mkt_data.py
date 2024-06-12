3fae897b87d60fe1fabf1a9110c94c3f65d36e42
pinouche <uriot.thomas@gmail.com>
2024-06-12 22:05:27 +0200

    change requestdata test

--- src/trading/core/api_actions/request_mkt_data.py
+++ src/trading/core/api_actions/request_mkt_data.py
@@ -7,14 +7,11 @@ from trading.api.ibapi_class import IBapi
 from trading.core.exceptions.checks import check_price_is_live_and_is_float
 
 
-def request_market_data(app: IBapi, ticker_symbol: str, nextorderid: int | None = None) -> None:
+def request_market_data(app: IBapi, ticker_symbol: str) -> None:
     """Request live streaming market data."""
-    if nextorderid is None:
-        nextorderid = app.nextorderId
-
     contract = get_stock_contract(ticker_symbol)
     # Snapshot is set to True, which means we only request a single data point (and not a stream of data).
-    app.reqMktData(nextorderid, contract, '', True, False, [])
+    app.reqMktData(app.nextorderId, contract, '', True, False, [])
     time.sleep(2)
-    check_price_is_live_and_is_float(app, nextorderid)
+    check_price_is_live_and_is_float(app, app.nextorderId)
     app.reqIds(-1)  # increment the next valid id (appl.nextorderId)
