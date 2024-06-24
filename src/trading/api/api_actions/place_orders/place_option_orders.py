747c1485aaa18834cba504366fefd00e31ae885f
pinouche <uriot.thomas@gmail.com>
2024-06-24 21:19:27 +0200

    add place options order function

--- src/trading/api/api_actions/place_orders/place_option_orders.py
+++ src/trading/api/api_actions/place_orders/place_option_orders.py
@@ -1,1 +1,12 @@
 """Script to place option order (buy or sell)"""
+
+from ibapi.contract import Contract
+from ibapi.order import Order
+
+from trading.api.ibapi_class import IBapi
+
+
+def place_option_order(app: IBapi, contract: Contract, order: Order) -> None:
+    """Place order to buy or sell an option (depending on the contract)."""
+    app.placeOrder(app.nextorderId, contract, order)
+    app.nextorderId += 1  # type: ignore
